80aa7d1aee777bd74c4bce730ed008b072b27d80
utsavvachhanicrickle <utsavvachhani.cs@gmail.com>
2026-04-22 12:14:43 +0530

    2204202602 - fixed the version 2 and also works with select option for person deatils of contact person and default Messages saved

--- client/src/components/SendMessage.jsx
+++ client/src/components/SendMessage.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import { messageServices } from "../services/message.services";
 import InputField from "./InputField";
+import Button from "./Button";
 
 function SendMessage({ sessionId }) {
   const [number, setNumber] = useState("");
   const [contactPerson, setContactPerson] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setData(JSON.parse(localStorage.getItem("messageData")) || []);
-  }, []);
+  const [data, setData] = useState(
+    JSON.parse(localStorage.getItem("messageData")) || [],
+  );
+  const [defaultMessage, setDefaultMessage] = useState("");
+  const [defaultMessagesOptions, setDefaultMessagesOptions] = useState(
+    JSON.parse(localStorage.getItem("defaultMessages")) || [],
+  );
 
   const sendMessage = async () => {
     if (!number || !message) {
@@ -19,14 +22,19 @@ function SendMessage({ sessionId }) {
       return;
     }
 
-    setData(data.find((item) => item.value === number) ? data : [...data, { label: contactPerson, value: number, message }]);
-    JSON.stringify(
-      localStorage.setItem(
-        "messageData",
-        JSON.stringify([...data, { label: contactPerson, value: number, message }]),
-      ),
+    console.log(
+      data.find((item) => item.value === number),
+      number,
     );
 
+    const exists = data.some((item) => item.value === number);
+
+    let updatedData = exists
+      ? data
+      : [...data, { label: contactPerson + " (" + number + ")", value: number }];
+
+    setData(updatedData);
+    localStorage.setItem("messageData", JSON.stringify(updatedData));
 
     try {
       setLoading(true);
@@ -42,66 +50,129 @@ function SendMessage({ sessionId }) {
   };
 
   const setSelectOption = (value) => {
-    console.log(value);
-    
     const selected = data.find((item) => item.value === value);
     if (selected) {
-      setContactPerson(selected.label);
+      setContactPerson(selected.label.split(" (")[0]);
       setNumber(selected.value);
       setMessage("");
     }
   };
+  const handlesDefaultMessageClicks = () => {
+    if (!defaultMessage) {
+      alert("Please enter a default message.");
+      return;
+    }
+
+    const exists = defaultMessagesOptions.some(
+      (item) => item.value === defaultMessage,
+    );
+
+    let updated = exists
+      ? defaultMessagesOptions
+      : [
+          ...defaultMessagesOptions,
+          { label: defaultMessage, value: defaultMessage },
+        ];
+
+    setDefaultMessagesOptions(updated);
+    localStorage.setItem("defaultMessages", JSON.stringify(updated));
+
+    setDefaultMessage("");
+  };
+
+  const handlesDefaultMessage = (value) => {
+    const selected = defaultMessagesOptions.find(
+      (item) => item.value === value,
+    );
+    setMessage(selected ? selected.value : "");
+    setDefaultMessage(value);
+  };
 
   return (
-    <div className="bg-(--card) p-6 rounded-2xl shadow-md border border-(--border) max-w-xl w-full mx-auto">
-      <h3 className="text-xl font-semibold mb-4 text-(--text-primary)">
-        Send Message
-      </h3>
+    <>
+      <div className="bg-(--card) p-6 rounded-2xl shadow-md border border-(--border) max-w-xl w-full mx-auto">
+        <h3 className="text-xl font-semibold mb-4 text-(--text-primary)">
+          Send Message
+        </h3>
 
-      <InputField
-        type="select"
-        placeholder="Select Contact Person"
-        name="selects"
-        value={contactPerson}
-        options={data}
-        onChange={(name, value) => setSelectOption(value)}
-      />
+        <InputField
+          type="select"
+          placeholder="Select Contact Person"
+          name="selects"
+          value={contactPerson}
+          options={data}
+          onChange={(name, value) => setSelectOption(value)}
+        />
 
-      <InputField
-        placeholder="Enter Contact Person Name"
-        value={contactPerson}
-        onChange={(name, value) => setContactPerson(value)}
-      />
+        <InputField
+          placeholder="Enter Contact Person Name"
+          value={contactPerson}
+          onChange={(name, value) => setContactPerson(value)}
+        />
 
-      {/* Phone Input */}
-      <InputField
-        placeholder="Enter phone number (without +91)"
-        value={number}
-        onChange={(name, value) => setNumber(value)}
-      />
+        {/* Phone Input */}
+        <InputField
+          placeholder="Enter phone number (without +91)"
+          value={number}
+          onChange={(name, value) => setNumber(value)}
+        />
 
-      {/* Message Input */}
-      <InputField
-        type="textarea"
-        placeholder="Enter your message..."
-        value={message}
-        onChange={(name, value) => setMessage(value)}
-        rows={4}
-      />
+        {/* Message Input */}
+        <InputField
+          type="textarea"
+          placeholder="Enter your message..."
+          value={message}
+          onChange={(name, value) => setMessage(value)}
+          rows={4}
+        />
 
-      {/* Button */}
-      <button
-        onClick={sendMessage}
-        disabled={loading}
-        className={`w-full py-3 rounded-lg font-semibold transition ${
-          loading
-            ? "bg-(--border) text-(--text-secondary) cursor-not-allowed"
-            : "bg-(--btn-primary-bg) text-(--btn-primary-text) hover:bg-(--btn-primary-hover)"
-        }`}
-      >
-        {loading ? "Sending..." : "Send Message"}
-      </button>
-    </div>
+        {/* Button */}
+        <button
+          onClick={sendMessage}
+          disabled={loading}
+          className={`w-full py-3 rounded-lg font-semibold transition ${
+            loading
+              ? "bg-(--border) text-(--text-secondary) cursor-not-allowed"
+              : "bg-(--btn-primary-bg) text-(--btn-primary-text) hover:bg-(--btn-primary-hover)"
+          }`}
+        >
+          {loading ? "Sending..." : "Send Message"}
+        </button>
+      </div>
+
+      <div className="bg-(--card) p-6 rounded-2xl shadow-md border border-(--border) max-w-xl w-full mx-auto">
+        <h3 className="text-xl font-semibold mb-4 text-(--text-primary)">
+          Set Default Message
+        </h3>
+        <InputField
+          type="select"
+          placeholder="Select Default Message Options"
+          name="selects"
+          value={defaultMessage}
+          options={defaultMessagesOptions}
+          onChange={(name, value) => handlesDefaultMessage(value)}
+        />
+
+        <InputField
+          placeholder="Enter your default message..."
+          value={defaultMessage}
+          onChange={(name, value) => setDefaultMessage(value)}
+        />
+
+        <Button
+          type="button"
+          onClick={handlesDefaultMessageClicks}
+          disabled={loading}
+          className={`w-full py-3 rounded-lg font-semibold transition ${
+            loading
+              ? "bg-(--border) text-(--text-secondary) cursor-not-allowed"
+              : "bg-(--btn-primary-bg) text-(--btn-primary-text) hover:bg-(--btn-primary-hover)"
+          }`}
+        >
+          {loading ? "Sending..." : "Send Message"}
+        </Button>
+      </div>
+    </>
   );
 }
 
