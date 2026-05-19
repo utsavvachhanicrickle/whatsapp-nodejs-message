94bb8630d7bf8f627f0aaacb866e5341a90171d0
utsavvachhanicrickle <utsavvachhani.cs@gmail.com>
2026-05-19 18:32:33 +0530

    1805202608 - api conected feture deafult keywords and keywords

---
+++ server/src/routes/defaultKeywordsMessages.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import { addContectController } from "../controllers/contact.controller.js";
+
+const router = express.Router();
+
+router.get("/sessionId/:id", authMiddleware, addContectController);
+router.post(
+  "/addsessionId/sessionId/:id",
+  authMiddleware,
+  addContectController,
+);
+router.put("/sessionId/:id/update/:id", authMiddleware, addContectController);
+router.delete(
+  "/sessionId/:id/delete/:id",
+  authMiddleware,
+  addContectController,
+);
+
+export default router;
--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -3,6 +3,8 @@ import userRoute from "./user.routes.js";
 import messageRoute from "./message.routes.js";
 import contactRoute from "./contact.routes.js";
 import defaultMessageRoute from "./defaultMessage.routes.js";
+import defaultKeywordsRoute from "./defaultKeywords.routes.js";
+import defaultKeywordsMessagesRoute from "./defaultKeywordsMessages.routes.js";
 import groupRoute from "./group.routes.js";
 
 const router = express.Router();
@@ -11,6 +13,8 @@ router.use("/user", userRoute);
 router.use("/message", messageRoute);
 router.use("/contact", contactRoute);
 router.use("/default-message", defaultMessageRoute);
+router.use("/default-keywords", defaultKeywordsRoute);
+router.use("/default-keywords-messages", defaultKeywordsMessagesRoute);
 router.use("/group", groupRoute);
 
 export default router;
